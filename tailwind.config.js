0e86340905275f2ebb3e2d72dccfa5f28a37a6b6
uwaisuabu001 <uwaisuabu001@gmail.com>
2026-04-20 01:39:05 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,17 @@
-
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        neutral: {
+          950: '#0a0a0a',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
