cd48a37cec6430816c4063d7e680a1a1bd602ecb
uwaisuabu001 <uwaisuabu001@gmail.com>
2026-04-20 01:31:05 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,14 @@
-
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <link rel="icon" type="image/svg+xml" href="/vite.svg" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Wapwit | Istanbul Street Style</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+  </head>
+  <body class="bg-neutral-950">
+    <div id="root"></div>
+    <script type="module" src="/src/main.jsx"></script>
+  </body>
+</html>
